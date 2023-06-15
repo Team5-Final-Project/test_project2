7adb9592cfb6bcc46e25a529458c67f2bc5d932c
ahtj1234@naver.com <ahtj1234@naver.com>
2023-06-15 14:40:46 +0900

    병

--- demo/src/main/java/com/example/demo/controller/HomeController.java
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	
+	// 커밋
 	@RequestMapping(value = "/home")
 	public String home() {
 		System.out.println("test");
